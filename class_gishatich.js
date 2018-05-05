c68d998c08995fb432e1ea004b0d985ea33457e0
Your Name <you@example.com>
2018-05-05 22:21:46 +0400

    gishatichi jarangakanutyun

--- class_gishatich.js
+++ class_gishatich.js
@@ -1,7 +1,6 @@
-class gishatich {
+class gishatich extends Kendani{
 	constructor(x, y, i) {
-		this.x = x;
-		this.y = y;
+		super(x,y);
 		this.e = 20;
 		this.b = 0;
 		this.index = i;
@@ -20,23 +19,14 @@ class gishatich {
 	}
 	yntrelVandak(ch) {
 		this.cord();
-		var found = [];
-		for (var i in this.directions) {
-			var x = this.directions[i][0];
-			var y = this.directions[i][1];
-			if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-				if (matrix[y][x] == ch) {
-					found.push(this.directions[i]);
-				}
-			}
-		}
-		return found;
+		return super.yntrelVandak(ch);
 	}
 	die() {
 		for (var i in gishatichner) {
 			if (gishatichner[i].x == this.x && gishatichner[i].y == this.y) {
 				gishatichner.splice(i, 1);
 				matrix[this.y][this.x] = 0;
+				break;
 			}
 		}
 	}
@@ -47,6 +37,7 @@ class gishatich {
 			for (var i in kover) {
 				if (kover[i].x == ran[0] && kover[i].y == ran[1]) {
 					kover.splice(i, 1);
+					break;
 				}
 			}
 			matrix[ran[1]][ran[0]] = 3;
